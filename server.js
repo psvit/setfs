53b3038a3fd7b155fd4bc6d3ce1a3c4baccc72af
attawit <psvit50@gmail.com>
2017-02-21 17:14:49 +0700

    fix port

--- server.js
+++ server.js
@@ -295,6 +295,6 @@ function IsJsonString(str) {
     return str;
   }
 
-app.listen('8081');
+app.listen(process.env.PORT || '8081');
 console.log('Magic happens on port 8081');
 exports = module.exports = app; 	
